dfe122527a2b1584d93544c2e5851844a47ff6b1
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2024-12-28 14:25:44 +0530

    fav

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -8,7 +8,7 @@ export function Favorites() {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 pt-24 pb-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-8 sm:px-6 lg:px-8">
         <div className="text-center mb-8" data-aos="fade-down">
           <h1 className="text-3xl font-extrabold text-gray-900 dark:text-white sm:text-4xl">
             Your Favorite Notes
